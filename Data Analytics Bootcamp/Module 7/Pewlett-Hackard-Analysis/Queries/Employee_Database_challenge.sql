96ef338ae126d9180addda031d13b22338f68fc9
Vantuil Junior <vlourenco.junior@outlook.com>
2022-08-11 23:23:56 -0400

    Preparing Files for Challenge Submission.

---
+++ Data Analytics Bootcamp/Module 7/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,42 @@
+-- https://github.com/Faye6155/Employee_Database_Challenge
+-- https://github.com/hemantkiri/Employee_Database
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+     e.first_name,
+     e.last_name,
+     t.title
+--INTO unique_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE(e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND(to_date = '9999-01-01')
+ORDER BY e.emp_no ASC, to_date DESC
+
+
+SELECT COUNT(ut.emp_no), t.title
+--INTO retiring_titles
+FROM unique_titles as ut
+LEFT JOIN titles as t
+ON ut.emp_no = t.emp_no
+GROUP BY t.title
+ORDER BY COUNT DESC
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+     e.first_name,
+     e.last_name,
+	 e.birth_date,
+     de.from_date,
+     de.to_date,
+	 t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+       AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC;
