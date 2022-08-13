1405a8e40f690fcd18166ebd2f85d07fa62cad75
Vantuil Junior <vlourenco.junior@outlook.com>
2022-08-12 21:10:35 -0400

    Adding the final files for submission.

--- Data Analytics Bootcamp/Module 7/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Data Analytics Bootcamp/Module 7/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -1,42 +1,51 @@
--- https://github.com/Faye6155/Employee_Database_Challenge
--- https://github.com/hemantkiri/Employee_Database
+--Making the Retirement Titles
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (e.emp_no) e.emp_no,
-     e.first_name,
-     e.last_name,
-     t.title
---INTO unique_titles
-FROM employees as e
-INNER JOIN titles as t
-ON (e.emp_no = t.emp_no)
-WHERE(e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
-AND(to_date = '9999-01-01')
-ORDER BY e.emp_no ASC, to_date DESC
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, from_date DESC;
 
+--Getting the number of retiring titles
+SELECT COUNT(emp_no),title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
 
-SELECT COUNT(ut.emp_no), t.title
---INTO retiring_titles
-FROM unique_titles as ut
-LEFT JOIN titles as t
-ON ut.emp_no = t.emp_no
-GROUP BY t.title
-ORDER BY COUNT DESC
-
-SELECT DISTINCT ON (e.emp_no) e.emp_no,
-     e.first_name,
-     e.last_name,
-	 e.birth_date,
-     de.from_date,
-     de.to_date,
-	 t.title
+--Mentorship Program
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+t.from_date,
+t.to_date,
+t.title
 INTO mentorship_eligibility
-FROM employees as e
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-INNER JOIN titles as t
+FROM employees AS e
+INNER JOIN titles AS t
 ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-       AND (de.to_date = '9999-01-01')
-ORDER BY e.emp_no ASC;
+WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+AND (t.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+--Getting the total amount of mentors for project analysis.
+SELECT COUNT(emp_no),title
+FROM mentorship_eligibility
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
