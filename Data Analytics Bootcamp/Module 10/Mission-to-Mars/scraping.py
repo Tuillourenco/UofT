8284f0fdb1f41bf0f1b8d73a64bfcf20b3f208e7
Vantuil Junior <vlourenco.junior@outlook.com>
2022-09-05 15:20:03 -0400

    Added the files for the project's closing out.

--- Data Analytics Bootcamp/Module 10/Mission-to-Mars/scraping.py
+++ Data Analytics Bootcamp/Module 10/Mission-to-Mars/scraping.py
@@ -1,67 +1,146 @@
 # Import Splinter, BeautifulSoup, and Pandas
+from http.server import executable
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
+import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+# Let's define this function as "scrape_all"
+def scrape_all():
 
-# Visit the Mars news site
-url = 'https://redplanetscience.com/'
-browser.visit(url)
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
 
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    news_title, news_paragraph = mars_news(browser)
 
-# Convert the browser html to a soup object and then quit the browser
-html = browser.html
-news_soup = soup(html, 'html.parser')
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now(),
+        "hemispheres": mars_hemispheres(browser)
+    }
 
-slide_elem = news_soup.select_one('div.list_text')
+    # Stop webdriver and return data
+    browser.quit()
 
-slide_elem.find('div', class_='content_title')
+    return data
 
-# Use the parent element to find the first a tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+# revisit that code and insert it into a function.
+def mars_news(browser):
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    # Scrape Mars News
+    # Visit the mars nasa news site
+    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+    browser.visit(url)
 
-# ## JPL Space Images Featured Image
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    except AttributeError:
+        return None, None
 
-# find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
+    return news_title, news_p
 
-# Use the base url to create an absolute url
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+# The code to scrape the featured image
+def featured_image(browser):
+    # Visit URL
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url)
 
-# ## Mars Facts
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.head()
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-df.columns=['Description', 'Mars', 'Earth']
-df.set_index('Description', inplace=True)
-df
+    # Add try/except for error handling
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 
-df.to_html()
+    except AttributeError:
+        return None
 
-browser.quit()
+    # Use the base url to create an absolute url
+    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+
+    return img_url
+
+# The code to scrape the Mars Facts.
+def mars_facts():
+    
+    # Add try/except for error handling
+    try:
+        # Use 'read_html' to scrape the facts table into a dataframe
+        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
+
+    except BaseException:
+        return None
+
+    # Assign columns and set index of dataframe
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
+
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html(classes="table table-striped")
+
+# ### Hemispheres
+def mars_hemispheres(browser):
+    # visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    # get the chunk of product code
+    product_soup = soup(browser.html, 'html.parser')
+    product_list = product_soup     .find('div', class_='collapsible results')     .find_all('div', 'item')
+
+    # for each product visit the page, scrape data
+    for product in product_list:
+        # get link for product and visit page    
+        link = product.find('a').get('href')
+        browser.visit(f"{url}{link}")
+        
+        # get the html
+        hemi_soup = soup(browser.html, 'html.parser')
+        
+        # get the image title (only 1 h2 on page)  
+        hemi_title = hemi_soup.find('h2').text
+        
+        # get the image link (seek an anchor link labeled Sample)
+        hemi_url = hemi_soup.find('a', string='Sample').get('href')
+        hemisphere_image_urls.append({
+            'img_url': f"{url}{hemi_url}",
+            'title': hemi_title })
+        
+    # return images and titles
+    return hemisphere_image_urls
+
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
--- Data Analytics Bootcamp/Module 10/README.md
+++ Data Analytics Bootcamp/Module 10/README.md
@@ -1,3 +1,21 @@
 ## Mission to Mars - Web Scraping with HTML/CSS
 
-### This project is for whom love astronomy and wishes to work collecting data on this subject one day; the team has decided to use a specific method of gathering the latest data: web scraping. Using this technique, she can pull data from multiple websites, store it in a database, then present the collected data in a central location: a webpage.
+### Project Scope:
+
+This project is for whom love astronomy and wishes to work collecting data on this subject one day; the team has decided to use a specific method of gathering the latest data: web scraping. Using this technique, she can pull data from multiple websites, store it in a database, then present the collected data in a central location: a webpage.
+As a deliverable, the web app is looking good and functioning well, and the team was polishing it. The images of Mars’s hemispheres online reached a good resolution, and the site is scraping-friendly. Therefore, some adjustments to the current web app were necessary to include all four of the hemisphere images added to the scope afterwards. To do this, it used BeautifulSoup and Splinter to scrape full-resolution images of Mars’s hemispheres and the titles of those images, store the scraped data on a Mongo database, use a web application to display the data and alter the design of the web app to accommodate these images.
+
+### Projet Deliverables:
+
+Following up on the project scope, as described in the previous section, it was assigned to the team three main deliverables consisting of three technical analyses:
+    - Deliverable 1: Scrape Full-Resolution Mars Hemisphere Images and Titles.
+    ![](Mission-to-Mars/images/report_img_01.png)
+
+    - Deliverable 2: Update the Web App with Mars Hemisphere Images and Titles.
+    ![](Mission-to-Mars/images/report_img_02.png)
+
+    - Deliverable 3: Add Bootstrap 3 Components.
+
+### Conclusion
+
+In conclusion, this project uses web scraping targets to build fast databases and extract insights from these industry-specific ones. Like all data analysis, these insights must solve business problems. Furthermore, it guides how to automate a web page to retrieve the desired data. The information collected in this project testifies its trustworthy data collection and performs clever data manipulation for analysis afterwards.
