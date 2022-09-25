549bd9ebf893fe3a1abf8e18bf4bdfcbc1b98467
Vantuil Junior <vlourenco.junior@outlook.com>
2022-09-25 18:21:58 -0400

    Adding Module 13 Folder.

---
+++ Data Analytics Bootcamp/Module 13/Mapping_Earthquakes/Mapping_GeoJSON_Multiple_Points/static/js/config.js
@@ -0,0 +1,2 @@
+// API Key
+const API_KEY = "pk.eyJ1IjoidHVpbGxvdXJlbmNvIiwiYSI6ImNsODlmYWNvYTA2ZmwzdWxrZ2w2eG5lZG8ifQ.eE4EW10doKQSCDqgcryZ4Q";
---
+++ Data Analytics Bootcamp/Module 13/Mapping_Earthquakes/Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,31 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create a map object
+var myMap = L.map("mapid", {
+    center: [40.7, -94.5],
+    zoom: 5
+  });
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(myMap);
+});
